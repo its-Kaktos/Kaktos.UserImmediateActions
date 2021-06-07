7d101b473d932f4d570c70ab9dd54eacf07420f0
ARMAN H <sticknumber123@gmail.com>
2021-06-07 12:05:30 +0430

    Remove Ctor fromAddOrRemoveClaimViewModel and ClaimsViewModel

--- samples/SampleIdentityMvc/Models/AddOrRemoveClaimViewModel.cs
+++ samples/SampleIdentityMvc/Models/AddOrRemoveClaimViewModel.cs
@@ -4,33 +4,12 @@ namespace SampleIdentityMvc.Models
 {
     public class AddOrRemoveClaimViewModel
     {
-        public AddOrRemoveClaimViewModel()
-        {
-            UserClaims = new List<ClaimsViewModel>();
-        }
-
-        public AddOrRemoveClaimViewModel(string userId, List<ClaimsViewModel> userClaims)
-        {
-            UserId = userId;
-            UserClaims = userClaims;
-        }
-
-
         public string UserId { get; set; }
         public List<ClaimsViewModel> UserClaims { get; set; }
     }
     
     public class ClaimsViewModel
     {
-        public ClaimsViewModel()
-        {
-        }
-
-        public ClaimsViewModel(string claimType)
-        {
-            ClaimType = claimType;
-        }
-
         public string ClaimType { get; set; }
         public bool IsSelected { get; set; }
     }
