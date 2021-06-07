9bc8a5369403c20b1ddecf17cb2bf11649a713d6
ARMAN H <sticknumber123@gmail.com>
2021-06-07 12:30:14 +0430

    Add AllUserClaimsViewModel

---
+++ samples/SampleIdentityMvc/Models/AllUserClaimsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SampleIdentityMvc.Models
+{
+    public class AllUserClaimsViewModel
+    {
+        public string ClaimType { get; set; }
+        public string ClaimValue { get; set; }
+    }
+}
