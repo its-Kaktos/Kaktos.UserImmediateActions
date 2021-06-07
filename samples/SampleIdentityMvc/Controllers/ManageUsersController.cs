2acec9a0ea1d39b26588dd8af44cf0cbf0a73601
ARMAN H <sticknumber123@gmail.com>
2021-06-07 13:06:22 +0430

    Update ManageUsersController.cs
    Add GET and POST AddClaimToUserWithOutImmediateUpdate endpoints

--- samples/SampleIdentityMvc/Controllers/ManageUsersController.cs
+++ samples/SampleIdentityMvc/Controllers/ManageUsersController.cs
@@ -93,5 +93,47 @@ namespace SampleIdentityMvc.Controllers
 
             return View(model);
         }
+        
+        [HttpGet]
+        public async Task<IActionResult> AddClaimToUserWithOutImmediateUpdate(string id)
+        {
+            if (string.IsNullOrEmpty(id)) return NotFound();
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) return NotFound();
+
+            return View(new AddClaimViewModel {UserId = id});
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddClaimToUserWithOutImmediateUpdate(AddClaimViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(model.UserId);
+                if (user == null) return NotFound();
+
+                var userClaims = await _userManager.GetClaimsAsync(user);
+                if (userClaims.Any(c=> c.Type == model.ClaimType && c.Value == model.ClaimValue))
+                {
+                    ModelState.AddModelError(string.Empty, "Claim already exists");
+                    return View(model);
+                }
+                
+                var result = await _userManager.AddClaimAsync(user, new Claim(model.ClaimType, model.ClaimValue));
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("AddClaimToUserWithOutImmediateUpdate", new {id = model.UserId});
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View(model);
+        }
     }
 }
