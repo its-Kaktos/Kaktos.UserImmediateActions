2178f4128377bdcc300ec2ceb49273367109bafe
ARMAN H <sticknumber123@gmail.com>
2021-06-07 16:13:58 +0430

    Add AddImmediateActionsFromDbToIImmediateActionsStore

---
+++ samples/SampleIdentityMvc/Services/AddImmediateActionsFromDbToIImmediateActionsStore.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Kaktos.UserImmediateActions.Models;
+using Kaktos.UserImmediateActions.Stores;
+using Microsoft.EntityFrameworkCore;
+using SampleIdentityMvc.Data;
+
+namespace SampleIdentityMvc.Services
+{
+    public class AddImmediateActionsFromDbToIImmediateActionsStore
+    {
+        private readonly IImmediateActionsStore _actionsStore;
+        private readonly ApplicationDbContext _dbContext;
+
+        public AddImmediateActionsFromDbToIImmediateActionsStore(IImmediateActionsStore actionsStore, ApplicationDbContext dbContext)
+        {
+            _actionsStore = actionsStore;
+            _dbContext = dbContext;
+        }
+
+        public async Task AddNonExpiredImmediateActionsFromDbToIImmediateActionsStoreAsync()
+        {
+            if (!await _dbContext.ImmediateActionDatabaseModels.AnyAsync()) return;
+
+            var nonExpiredImmediateActions = await _dbContext.ImmediateActionDatabaseModels
+                .Where(i => i.ExpirationTime > DateTime.Now)
+                .ToListAsync();
+
+            foreach (var immediateAction in nonExpiredImmediateActions)
+            {
+                var expirationTime = immediateAction.ExpirationTime - DateTime.Now;
+                var data = new ImmediateActionDataModel(immediateAction.AddedDate, immediateAction.Purpose);
+                
+                await _actionsStore.AddAsync(immediateAction.ActionKey,
+                    expirationTime,
+                    data);
+            }
+        }
+    }
+}
