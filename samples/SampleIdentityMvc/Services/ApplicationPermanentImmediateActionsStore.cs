a11dab9d89ea0bae076b9da2d37292ec6c146919
ARMAN H <sticknumber123@gmail.com>
2021-06-07 14:13:54 +0430

    Add ApplicationPermanentImmediateActionsStore

---
+++ samples/SampleIdentityMvc/Services/ApplicationPermanentImmediateActionsStore.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Kaktos.UserImmediateActions.Models;
+using Kaktos.UserImmediateActions.Stores;
+using SampleIdentityMvc.Data;
+using SampleIdentityMvc.Models;
+
+namespace SampleIdentityMvc.Services
+{
+    public class ApplicationPermanentImmediateActionsStore : IPermanentImmediateActionsStore
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ApplicationPermanentImmediateActionsStore(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Add(string key, DateTime expirationTime, ImmediateActionDataModel data)
+        {
+            if (string.IsNullOrEmpty(key)) throw new ArgumentException("Value cannot be null or empty.", nameof(key));
+            if (data == null) throw new ArgumentNullException(nameof(data));
+
+            var model = new ImmediateActionDatabaseModel
+            {
+                ActionKey = key,
+                ExpirationTime = expirationTime,
+                Purpose = data.Purpose,
+                AddedDate = data.AddedDate
+            };
+
+            _dbContext.ImmediateActionDatabaseModels.Add(model);
+        }
+
+        public async Task AddAsync(string key, DateTime expirationTime, ImmediateActionDataModel data, CancellationToken cancellationToken = default)
+        {
+            var model = new ImmediateActionDatabaseModel
+            {
+                ActionKey = key,
+                ExpirationTime = expirationTime,
+                Purpose = data.Purpose,
+                AddedDate = data.AddedDate
+            };
+
+            await _dbContext.ImmediateActionDatabaseModels.AddAsync(model, cancellationToken);
+        }
+    }
+}
