95c19ad3d65df9b24ab8e3b931a6e303efc83413
ARMAN H <sticknumber123@gmail.com>
2021-06-07 11:53:09 +0430

    Inject usermanager to ManageUsersController.cs

--- samples/SampleIdentityMvc/Controllers/ManageUsersController.cs
+++ samples/SampleIdentityMvc/Controllers/ManageUsersController.cs
@@ -1,9 +1,17 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SampleIdentityMvc.Controllers
 {
     public class ManageUsersController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ManageUsersController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
