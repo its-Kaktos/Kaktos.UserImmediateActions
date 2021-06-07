44de80a62b48482e1d976ab10b94e61a72c8946b
ARMAN H <sticknumber123@gmail.com>
2021-06-07 12:11:10 +0430

    Add required attribute for AddOrRemoveClaimViewModel

--- samples/SampleIdentityMvc/Models/AddOrRemoveClaimViewModel.cs
+++ samples/SampleIdentityMvc/Models/AddOrRemoveClaimViewModel.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SampleIdentityMvc.Models
 {
     public class AddOrRemoveClaimViewModel
     {
+        [Required]
         public string UserId { get; set; }
+        
+        [Required]
         public List<ClaimsViewModel> UserClaims { get; set; }
     }
     
     public class ClaimsViewModel
     {
+        [Required]
         public string ClaimType { get; set; }
+        
+        [Required]
         public string ClaimValue { get; set; }
+        
+        [Required]
         public bool IsSelected { get; set; }
     }
 }
