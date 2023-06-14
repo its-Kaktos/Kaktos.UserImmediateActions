462bf56b3feabea48eaf4b4cc302473eee9c4383
kaktos <sticknumber123@gmail.com>
2023-06-14 20:10:25 +0330

    docs: Add comment to constructor of ImmediateActionDataModel

--- src/Kaktos.UserImmediateActions/Models/ImmediateActionDataModel.cs
+++ src/Kaktos.UserImmediateActions/Models/ImmediateActionDataModel.cs
@@ -4,6 +4,11 @@ namespace Kaktos.UserImmediateActions.Models
 {
     public class ImmediateActionDataModel
     {
+        /// <summary>
+        /// Constructs new instance of <see cref="ImmediateActionDataModel"/>
+        /// </summary>
+        /// <param name="addedDate">Added date of this model, typically its current date time</param>
+        /// <param name="purpose">Purpose or what operation needs to be done on user</param>
         public ImmediateActionDataModel(DateTimeOffset addedDate, AddPurpose purpose)
         {
             AddedDate = addedDate;
