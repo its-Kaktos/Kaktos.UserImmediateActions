be4c48d1b40b556e64bfd4b8d53534f373d967fd
kaktos <sticknumber123@gmail.com>
2023-06-14 07:23:54 +0330

    Comment(AddPurpose): Add comments

--- src/Kaktos.UserImmediateActions/AddPurpose.cs
+++ src/Kaktos.UserImmediateActions/AddPurpose.cs
@@ -2,9 +2,24 @@ namespace Kaktos.UserImmediateActions
 {
     public enum AddPurpose
     {
+        /// <summary>
+        /// Refresh cookie
+        /// </summary>
         RefreshCookie,
+
+        /// <summary>
+        /// Sing out
+        /// </summary>
         SignOut,
+
+        /// <summary>
+        /// User was signed out
+        /// </summary>
         UserWasSignedOut,
+
+        /// <summary>
+        /// User cookie was refreshed
+        /// </summary>
         UserCookieWasRefreshed
     }
 }
