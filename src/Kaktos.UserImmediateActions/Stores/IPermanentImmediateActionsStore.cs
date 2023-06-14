b16b018304fa411b92204f1f183f4b71cba05e37
kaktos <sticknumber123@gmail.com>
2023-06-14 20:30:38 +0330

    feat: Change DateTime to DateTimeOffset in IPermanentImmediateActionsStore

--- src/Kaktos.UserImmediateActions/Stores/IPermanentImmediateActionsStore.cs
+++ src/Kaktos.UserImmediateActions/Stores/IPermanentImmediateActionsStore.cs
@@ -16,9 +16,9 @@ namespace Kaktos.UserImmediateActions.Stores
         /// </para>
         /// </summary>
         /// <param name="key">The key that is passed to the Add method of <see cref="IImmediateActionsStore"/>.</param>
-        /// <param name="expirationTime">Expiration time that is passed to the Add method of <see cref="IImmediateActionsStore"/>.</param>
+        /// <param name="expirationTimeUtc">Expiration time that is passed to the Add method of <see cref="IImmediateActionsStore"/>.</param>
         /// <param name="data">The data that is passed to the Add method of <see cref="IImmediateActionsStore"/>.</param>
-        void Add(string key, DateTime expirationTime, ImmediateActionDataModel data);
+        void Add(string key, DateTimeOffset expirationTimeUtc, ImmediateActionDataModel data);
 
         /// <summary>
         /// This method gets called inside the AddAsync method of <see cref="IImmediateActionsStore"/>.
@@ -29,10 +29,10 @@ namespace Kaktos.UserImmediateActions.Stores
         /// </para>
         /// </summary>
         /// <param name="key">The key that is passed to the AddAsync method of <see cref="IImmediateActionsStore"/>.</param>
-        /// <param name="expirationTime">Expiration time that is passed to the AddAsync method of <see cref="IImmediateActionsStore"/>.</param>
+        /// <param name="expirationTimeUtc">Expiration time that is passed to the AddAsync method of <see cref="IImmediateActionsStore"/>.</param>
         /// <param name="data">The data that is passed to the AddAsync method of <see cref="IImmediateActionsStore"/>.</param>
         /// <param name="cancellationToken"><see cref="CancellationToken"/></param>
         /// <returns><see cref="Task"/></returns>
-        Task AddAsync(string key, DateTime expirationTime, ImmediateActionDataModel data, CancellationToken cancellationToken = default);
+        Task AddAsync(string key, DateTimeOffset expirationTimeUtc, ImmediateActionDataModel data, CancellationToken cancellationToken = default);
     }
 }
