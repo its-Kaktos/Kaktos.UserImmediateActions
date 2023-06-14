c84530a253874b0c490ba43818bb18487c5a1e75
kaktos <sticknumber123@gmail.com>
2023-06-14 18:44:05 +0330

    Comment(IUserImmediateActionsService): Update comments to be more readable

--- src/Kaktos.UserImmediateActions/IUserImmediateActionsService.cs
+++ src/Kaktos.UserImmediateActions/IUserImmediateActionsService.cs
@@ -6,35 +6,39 @@ namespace Kaktos.UserImmediateActions
     public interface IUserImmediateActionsService
     {
         /// <summary>
-        /// Adds <paramref name="userId"/> to the configured store (E.g: Redis cache) so the next time user with the
-        /// given <paramref name="userId"/> sends a request, their cookie will be refreshed.
+        /// Refresh cookie AKA regenerate cookie of the user with the given <paramref name="userId"/> next time
+        /// they sends a request
         /// </summary>
-        /// <param name="userId">User id to refresh their cookie.</param>
+        /// <param name="userId">User id to refresh their cookie</param>
         void RefreshCookie(string userId);
 
         /// <summary>
-        /// Adds <paramref name="userId"/> to the configured store (E.g: Redis cache) so the next time user with the
-        /// given <paramref name="userId"/> sends a request, their cookie will be refreshed.
+        /// Refresh cookie AKA regenerate cookie of the user with the given <paramref name="userId"/> next time
+        /// they sends a request Asynchronously
         /// </summary>
-        /// <param name="userId">User id to refresh their cookie.</param>
+        /// <param name="userId">User id to refresh their cookie</param>
         /// <param name="cancellationToken"><see cref="CancellationToken"/></param>
-        /// <returns><see cref="Task"/></returns>
+        /// <returns>The task object representing the asynchronous operation.</returns>
         Task RefreshCookieAsync(string userId, CancellationToken cancellationToken = default);
 
         /// <summary>
-        /// Adds <paramref name="userId"/> to the configured store (E.g: Redis cache) so the next time user with the
-        /// given <paramref name="userId"/> sends a request, they will be signed out from their account.
+        /// Sings out the user with the given <paramref name="userId"/> next time
+        /// they sends a request
         /// </summary>
-        /// <param name="userId">User id to refresh their cookie.</param>
+        /// <remarks>It is recommended to update user Security Stamp when calling this method to ensure
+        /// sign out on all devices</remarks>
+        /// <param name="userId">The user id to log out of their account</param>
         void SignOut(string userId);
 
         /// <summary>
-        /// Adds <paramref name="userId"/> to the configured store (E.g: Redis cache) so the next time user with the
-        /// given <paramref name="userId"/> sends a request, they will be signed out from their account.
+        /// Sings out the user with the given <paramref name="userId"/> next time
+        /// they sends a request Asynchronously
         /// </summary>
-        /// <param name="userId">User id to refresh their cookie.</param>
+        /// <remarks>It is recommended to update user Security Stamp when calling this method to ensure
+        /// sign out on all devices</remarks>
+        /// <param name="userId">The user id to log out of their account</param>
         /// <param name="cancellationToken"><see cref="CancellationToken"/></param>
-        /// <returns><see cref="Task"/></returns>
+        /// <returns>The task object representing the asynchronous operation.</returns>
         Task SignOutAsync(string userId, CancellationToken cancellationToken = default);
     }
 }
