026ef26ef7f257db7939163a50cd0f71e9022968
kaktos <sticknumber123@gmail.com>
2023-06-15 11:17:34 +0330

    migrations: add new migration to change datetime to DateTimeOffset and add suffix Utc to the date type columns

---
+++ samples/SampleIdentityMvc/Data/Migrations/20230615074550_change_datetime_to_datetimeoffset.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SampleIdentityMvc.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class change_datetime_to_datetimeoffset : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ExpirationTime",
+                table: "ImmediateActionDatabaseModels",
+                newName: "ExpirationTimeUtc");
+
+            migrationBuilder.RenameColumn(
+                name: "AddedDate",
+                table: "ImmediateActionDatabaseModels",
+                newName: "AddedDateUtc");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ExpirationTimeUtc",
+                table: "ImmediateActionDatabaseModels",
+                newName: "ExpirationTime");
+
+            migrationBuilder.RenameColumn(
+                name: "AddedDateUtc",
+                table: "ImmediateActionDatabaseModels",
+                newName: "AddedDate");
+        }
+    }
+}
